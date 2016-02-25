41407fff968ef8c90dd383bb6ad511bc0d7be734
rostopira <rostopiradv@gmail.com>
2016-02-25 21:52:56 +0200

    I forgot, what have done. But it's working

--- src/main/java/ua/rostopira/virtualpointerserver/PointerService.java
+++ src/main/java/ua/rostopira/virtualpointerserver/PointerService.java
@@ -3,8 +3,9 @@ package ua.rostopira.virtualpointerserver;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
@@ -17,23 +18,8 @@ import android.view.ViewConfiguration;
 import android.view.WindowManager;
 
 public class PointerService extends Service {
-    OverlayView mView;
+    OverlayView overlayView;
     UDPListener listener;
-    CountDownTimer timer;
-
-    public void Update(final int x, final int y) {
-        Log.d("PointerService", "Updating cursor position. X = " + Integer.toString(x) +
-                "\nY = " + Integer.toString(y));
-        mView.mShowCursor = true;
-        timer.cancel();
-        timer.start();
-        mView.Update(x, y);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 
     @Override
     public void onCreate() {
@@ -45,28 +31,19 @@ public class PointerService extends Service {
         Point screenSize = new Point();
         wm.getDefaultDisplay().getRealSize(screenSize); //Yeah, that's why I support only 4.2+
         //Just joking. I support only 4.2+, because the first Android HDMI stick used 4.2.2,
-        //and almost all of them currently updated to KitKat
-        //Have an older version of Android? Really? Use CM or just throw out that mammoth shit
+        //Have an older version of Android? Really? Use CM or just throw away that mammoth shit
 
-        mView = new OverlayView(this);
+        overlayView = new OverlayView(this);
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 screenSize.x, screenSize.y,
                 WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 PixelFormat.TRANSPARENT);
         params.gravity = Gravity.TOP | Gravity.LEFT;
         params.setTitle("Cursor");
-        wm.addView(mView, params);
-
-        timer = new CountDownTimer(5000, 5000) {
-            @Override public void onTick(long l) {}
-            @Override public void onFinish() {
-                mView.mShowCursor = false;
-            }
-        };
-        timer.start();
+        wm.addView(overlayView, params);
 
         Singleton.getInstance().screenSize = screenSize;
         Singleton.getInstance().longPress = Integer.toString(ViewConfiguration.getLongPressTimeout());
@@ -76,49 +53,70 @@ public class PointerService extends Service {
         Log.d("PointerService", "Service created");
     }
 
+    public void Update(final int x, final int y) {
+        Log.d("PointerService", "Updating cursor position. X = " + Integer.toString(x) +
+                "\nY = " + Integer.toString(y));
+        overlayView.Update(x, y);
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d("CursorService", "Service destroyed");
         Singleton.getInstance().pointerService = null;
-        if(mView != null) {
-            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(mView);
-            mView = null;
+        if(overlayView != null) {
+            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(overlayView);
+            overlayView = null;
         }
         listener.stop();
     }
 
     /**
      * Long time ago there was some code from pocketmagic.com
-     * And that was horrible shit
-     * So I rewrote it
+     * And that was horrible, so I fully rewrote it
      * But thanks for idea
      */
-
     class OverlayView extends View {
-        private Paint mLoadPaint;
-        public boolean mShowCursor;
-        public int x = 0,y = 0;
-
-        public void Update(int nx, int ny) {
-            x = nx;
-            y = ny;
-            postInvalidate();
-        }
+        boolean showCursor;
+        int x = 0,y = 0;
+        CountDownTimer timer;
+        Bitmap cursor;
+        Paint paint;
 
         public OverlayView(Context context) {
             super(context);
-            mLoadPaint = new Paint();
-            mLoadPaint.setColor(Color.MAGENTA);
+            timer = new CountDownTimer(3000, 3000) {
+                @Override public void onTick(long l) {}
+                @Override public void onFinish() {
+                    showCursor = false;
+                    postInvalidate();
+                }
+            };
+            showCursor = false;
+            cursor = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_cursor);
+            paint = new Paint();
+        }
+
+        public void Update(int X, int Y) {
+            showCursor = true;
+            timer.cancel();
+            timer.start();
+            x = X;
+            y = Y;
+            postInvalidate();
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            if (mShowCursor)
-                canvas.drawCircle(x,y,20,mLoadPaint); //TODO: replace dot with cursor
+            if (showCursor)
+                canvas.drawBitmap(cursor,x,y,paint);
         }
     }
 
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
 
